972aa3ce738c5b3787e74bd2a55c8e3ee6e556a3
rtpro5626 <136262363+rtpro5626@users.noreply.github.com>
2023-07-27 10:20:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// Crea una referencia para el elemento canvas
+canvas = new fabric.Canvas("myCanvas");
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //define una variable para una imagen nueva
+	img_imgTag.onload = uploadimg; // establece una función para cargar la variable
+	img_imgTag.src = img_image;   // carga la imagen
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+//Escribe el código para obtener el evento key-pressed
+var keyPressed
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		//Escribe el código para verificar el tipo de tecla que se presionó
+		console.log(keyPressed)
+
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey()
+{
+	//Sube la imagen con el mensaje. 
+	img_image="Alpkey.png";
+	add();
+}
+function numberkey()
+{
+	img_image="numberkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
